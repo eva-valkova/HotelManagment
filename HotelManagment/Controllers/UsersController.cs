2e2a5a471179d7ae3ec3f9cee2d30945d08bb442
Eva Valkova <eva.n.valkova@gmail.com>
2026-03-30 07:57:40 +0300

    usercontroller

---
+++ HotelManagment/Controllers/UsersController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelManagment.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HotelManagment.Controllers
+{
+    [Authorize(Roles = "Admin")] 
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string searchString, int? pageSize, int? pageNumber)
+        {
+            var users = _userManager.Users.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(u => u.UserName.Contains(searchString) ||
+                                       u.Email.Contains(searchString));
+            }
+
+            int selectedPageSize = pageSize ?? 10;
+            int pageIndex = pageNumber ?? 1;
+
+            var pagedList = await users
+                .Skip((pageIndex - 1) * selectedPageSize)
+                .Take(selectedPageSize)
+                .ToListAsync();
+
+            ViewBag.CurrentPageSize = selectedPageSize;
+            return View(pagedList);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.IsActive = false;
+                user.DismissalDate = DateTime.Now;
+                await _userManager.UpdateAsync(user);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ApplicationUser model, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email, 
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName,
+                    EGN = model.EGN,
+                    PhoneNumber = model.PhoneNumber,
+                    AppointmentDate = model.AppointmentDate,
+                    IsActive = true
+                };
+
+                var result = await _userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "Employee");
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null) return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+
+    }
+}
--- HotelManagment/obj/Debug/net8.0/HotelManagment.AssemblyInfo.cs
+++ HotelManagment/obj/Debug/net8.0/HotelManagment.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481755bf420d72cd570102698f11795ce1dc6680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4693c5aa78b7dbc2644722f38783b06636b977b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
