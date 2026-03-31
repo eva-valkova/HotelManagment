aadb616e8f2eb82fad691db0e4148272b91372f9
Eva Valkova <eva.n.valkova@gmail.com>
2026-03-31 08:47:47 +0300

    clientcontroller

--- HotelManagment/Data/ApplicationDbContext.cs
+++ HotelManagment/Data/ApplicationDbContext.cs
@@ -11,27 +11,33 @@ namespace HotelManagment.Data
         {
         }
 
-        public DbSet<Client> Clients { get; set; }
         public DbSet<Room> Rooms { get; set; }
+        public DbSet<Client> Clients { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
-
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-           builder.Entity<Room>()
-                .HasIndex(r => r.RoomNumber)
-                .IsUnique();
-
             builder.Entity<Reservation>()
                 .HasMany(r => r.Clients)
-                .WithMany(c => c.Reservations)
-                .UsingEntity(j => j.ToTable("ReservationClients"));
+                .WithMany(c => c.Reservations);
 
-            builder.Entity<Room>().Property(r => r.PriceForAdult).HasPrecision(18, 2);
-            builder.Entity<Room>().Property(r => r.PriceForChild).HasPrecision(18, 2);
-            builder.Entity<Reservation>().Property(r => r.TotalAmount).HasPrecision(18, 2);
+            builder.Entity<Room>(entity =>
+            {
+                entity.Property(e => e.PriceForAdult).HasPrecision(18, 2);
+                entity.Property(e => e.PriceForChild).HasPrecision(18, 2);
+            });
+
+            builder.Entity<Reservation>(entity =>
+            {
+                entity.Property(e => e.TotalAmount).HasPrecision(18, 2);
+
+                entity.HasOne(r => r.ReservedRoom)
+                      .WithMany()
+                      .HasForeignKey(r => r.RoomId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
